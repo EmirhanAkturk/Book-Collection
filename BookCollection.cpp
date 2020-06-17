0a84c7186b6a0888f40b03046ede39763dab4704
Emirhan Aktürk <61085526+EmirhanAkturk@users.noreply.github.com>
2020-06-17 22:23:39 +0300

    comment lines edited

--- BookCollection.cpp
+++ BookCollection.cpp
@@ -7,8 +7,6 @@ using namespace std;
 class Item
 {
 public:
-	// Verilen parametrelere uygun şekilde yeni bir item nesnesi oluşturur [5 Puan]
-	// Constructs a new Item object accoring to the given parameters [5 points]
 	Item(const string& name, int count = 1){
         this->name=name;
         this->count=count;
@@ -24,11 +22,7 @@ public:
 
 class BookCollection
 {
-	/* Koleksiyonda bulunan tum ogeleri ekrana bastirin [5 Puan]
-	   (iomanip, vb.) ek kutuphaneler yardimiyla tablo şeklinde bir gorunum olusturun [5 Ek Puan]*/
-
-	   /* prints all the items in the collection [5 point]
-		   use extra libraries (iomanip, etc.) to create a table view [5 points extra]	*/
+	
 	friend ostream& operator<<(ostream & out, const BookCollection & collection){
 
         for(int i{0};i<collection.numberOfUniqueItems;++i)
@@ -39,11 +33,9 @@ class BookCollection
 public:
 	BookCollection():numberOfUniqueItems{0},items{nullptr} { } // empty collection/ bos koleksiyon -> [5 puan/points]
     
-	// 1 item of bookname [5 points] 
-	// verilen kitap isminden tek kitap iceren bir koleksiyon olusturur [5 puan]
 	BookCollection(const string & bookname){
         
-        //cout<<"string conversion constructor"<<endl;
+      
         numberOfUniqueItems=1;
         items=new Item[numberOfUniqueItems];
         if(items==nullptr)
@@ -64,29 +56,18 @@ public:
         
         items[0]=item;
 
-    } // creates a collection of 1 item given -> 5 points
-
-	/* !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-		ASAGI EKSIK OLAN PROTOTOPLERI TAMAMLAYINIZ!!!
-		COMPLETE THE MISSING FUNCTION PROTOTYPES BELOW!!!
-		!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!*/
-		
-        //  Write a destructor [5 points]
-		//  Yikici fonksiyon [5 Puan]
+    } 
     ~BookCollection(){
         
-        //cout<<"Destructor\n";
         if(items!=nullptr){
             delete[] items;
             items=nullptr;}
         
     }
 		
-        // Write a copy constructor -> 10 points
-		// Kopya Yapici [10 Puan]
+        
     BookCollection(BookCollection& otherCollection):numberOfUniqueItems{otherCollection.numberOfUniqueItems}
     {
-        //cout<<"Copy constructer"<<endl;
         
         items=new Item[numberOfUniqueItems];
         
@@ -99,8 +80,7 @@ public:
     }
 
 
-		// Write a move constructor -> 10 points
-		// Tasiyici Yapici [10 puan]
+		
     BookCollection(BookCollection&& otherCollection)
     :numberOfUniqueItems{otherCollection.numberOfUniqueItems},items{otherCollection.items}
     {
@@ -110,8 +90,6 @@ public:
     }
 
 
-		// Overload copy assignment operator -> 10 points
-		// Kopyalayici Atama Operatorunu asırı yukleyin [10 Puan]
     BookCollection & operator=(const BookCollection & otherCollection){
         cout<<"copy assignmet"<<endl;
         if(this!= &otherCollection){//selt assignmet check
@@ -131,8 +109,7 @@ public:
     }
 
 
-		// Overload move assignment operator -> 10 points
-		// Tasıyıcı Atama Operatorunu asırı yukleyin [10 Puan]
+		
     BookCollection & operator=(BookCollection &&otherCollection){
         cout << "Move Assignment" << endl;
 
@@ -150,20 +127,11 @@ public:
         return *this;
     }
 
-		// Koleksiyon bossa true, degilse false dondurur
-		// ! operator returns true if the collection is empty and it results false otherwise.
+		
 	bool operator!() const{
         return numberOfUniqueItems==0;
-    } // [3 puan/points]
-
-
-	/* ==, != , >, <, >= ve <= operatorlerini asırı yukleyin
-	   Bu operatorler Koleksiyon icindeki TOPLAM OGE sayisina gore karsılastırma yaparlar.
-	   Tum operatorleri dogru yapanlar [22 Puan], diger durumda operator basına [3 Puan]  */
-	   /* Overload ==, != , >, <, >= and <= operators
-		  These operators compare the TOTAL NUMBER OF ITEMS (counts of all items) in the collections
-		  Total points of comparison operators
-		  (6 operators = 22 points, less than 6 operators each operator is 3 points)*/
+    } 
+	
     bool operator==(const BookCollection & otherCollection)const{
         if(numberOfUniqueItems!=otherCollection.numberOfUniqueItems)
             return false;
@@ -202,14 +170,6 @@ public:
     }
 
 
-
-
-    /* İki koleksiyonu birleştiren += operatorünü aşırı yükleyiniz [25 Puan]
-			 Eğer bir öğe mevcut koleksiyonda yoksa o öğeyi ekleyin,
-			 eğer öğe zaten mevcutsa adedini güncelleyin.*/
-			 /* Overload += operator which merges two collections [25 points]
-			 / if an item in the otherCollection is already in the collection than only increase the item count
-			 / otherwise add the item as a new item*/
 	BookCollection& operator+=(const BookCollection& otherCollection){
         int tempUniqueItems=numberOfUniqueItems+otherCollection.numberOfUniqueItems;//max duruma göre alan ayırıldı
         int isFind;
@@ -255,15 +215,6 @@ public:
         return *this;
     }
 
-	/* Koleksiyondan bir diğer koleksiyonu çıkartan -= operatörünü aşırı yükleyin [30 Puan]
-	  Eğer çıkartılacak öğe koleksiyonda yoksa bir şey yapılmaz,
-	  çıkartılacak öğenin adedi mevcuttan fazlaysa/mevcuda eşitse öğe koleksiyondan çıkartılır,
-	  çıkartılacak öğenin adedi mevcuttan azsa sadece adet güncellenir.
-	  */
-	  /* Overload -= operator which removes an item from the collection
-	   if an item that you need to remove is not a member of the collection than ignore that item
-	   if given item has equal or more number of instances than the collection has
-	   then you must completly remove the item from the collection otherwise only update the item count*/
 	BookCollection& operator-=(const Item& item){
         
         for(int i{0};i<numberOfUniqueItems;++i){//silinecek item collection içinde var mı diye kontrol eder
@@ -282,7 +233,7 @@ public:
         }
 
         return *this;
-    } // 30 points
+    } 
 
 
 private:
@@ -291,9 +242,7 @@ private:
 	int numberOfUniqueItems=0;
 	Item * items;
 
-	// Isterseniz buraya yardımcı fonksiyonlar yazabilirsiniz.
-	// If you want you can write helper functions here.
-
+	
 };
 
 int main(){
